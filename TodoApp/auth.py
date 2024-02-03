cb16ef98daadffd2423c22f1b8faffc3dd5e518b
evaristofm <evaristofm@gmail.com>
2024-02-03 12:04:13 -0300

    Criando arquivo separado com todas as configurações de authenticação

---
+++ TodoApp/auth.py
@@ -0,0 +1,50 @@
+from typing import Annotated
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+from datetime import timedelta, datetime
+from passlib.context import CryptContext
+from fastapi import Depends, status, HTTPException
+
+from fastapi.security import OAuth2PasswordBearer
+
+from models import User
+from database import get_db
+
+
+SECRET_KEY = 'alklzmdf'
+ALGOTITHM = 'HS256'
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+
+
+def create_acess_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGOTITHM)
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGOTITHM])
+        username: str = payload.get('sub')
+        user_id: str = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail='Could not validate user.')
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could no validate user.')
--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,54 +1,13 @@
 from typing import Annotated
-from jose import jwt, JWTError
-from sqlalchemy.orm import Session
-from datetime import timedelta, datetime
+from datetime import timedelta
 from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 from models import User
-from database import get_db
 from schemas import CreateUserRequest, Token
+from auth import bcrypt_context, authenticate_user, create_acess_token, db_dependency
 
 router = APIRouter(prefix='/auth', tags=['auth'])
-
-SECRET_KEY = 'alklzmdf'
-ALGOTITHM = 'HS256'
-
-bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
-
-db_dependency = Annotated[Session, Depends(get_db)]
-
-
-def authenticate_user(username: str, password: str, db: db_dependency):
-    user = db.query(User).filter(User.username == username).first()
-    if not user:
-        return False
-    if not bcrypt_context.verify(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_acess_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {'sub': username, 'id': user_id}
-    expires = datetime.utcnow() + expires_delta
-    encode.update({'exp': expires})
-    return jwt.encode(encode, SECRET_KEY, algorithm=ALGOTITHM)
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGOTITHM])
-        username: str = payload.get('sub')
-        user_id: str = payload.get('id')
-        if username is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail='Could not validate user.')
-        return {'username': username, 'id': user_id}
-    except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Could no validate user.')
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
